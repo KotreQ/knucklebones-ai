a931721de5dc9ce1bbfcc1d5ee416f6efad8fd0f
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-09-14 18:05:42 +0200

    Changed player in Action to ActionType

--- knucklebones_ai/game/state.py
+++ knucklebones_ai/game/state.py
@@ -10,9 +10,13 @@ class Player(Enum):
     OPPONENT = 3
 
 
+class ActionType(Enum):
+    ROLL = 0
+    MOVE = 1
+
 @dataclass(frozen=True, slots=True)
 class Action:
-    player: Player
+    action_type: ActionType
     value: int
 
 
@@ -25,17 +29,20 @@ class State:
 
     def get_actions(self) -> list[Action]:
         if self.player in (Player.PLAYER_ROLL, Player.OPPONENT_ROLL):
-            return [Action(self.player, val) for val in range(1, 7)]
+            return [Action(ActionType.ROLL, val) for val in range(1, 7)]
         else:
             board = self.player_board if self.player == Player.PLAYER else self.opponent_board
-            return [Action(self.player, col) for col in range(3) if board.is_col_available(col)]
+            return [Action(ActionType.MOVE, col) for col in range(3) if board.is_col_available(col)]
 
     def transition(self, action: Action) -> State:
-        assert action.player == self.player
+        if action.action_type == ActionType.ROLL:
+            assert self.player in (Player.PLAYER_ROLL, Player.OPPONENT_ROLL)
+        else:
+            assert self.player is (Player.PLAYER, Player.OPPONENT)
 
         new_player = Player(self.player.value + 1 % len(Player))
 
-        if self.player in (Player.PLAYER_ROLL, Player.OPPONENT_ROLL):
+        if action.action_type == ActionType.ROLL:
             new_roll = action.value
             return State(new_player, new_roll, self.player_board.copy(), self.opponent_board.copy())
 
