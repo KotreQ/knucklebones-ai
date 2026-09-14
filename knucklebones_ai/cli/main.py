f8600f7308185b294c773fecbd5a50a253681a4e
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-09-14 20:30:31 +0200

    Added cli command to create a new game

--- knucklebones_ai/cli/main.py
+++ knucklebones_ai/cli/main.py
@@ -1,10 +1,18 @@
 import os
 import subprocess as sp
-from knucklebones_ai.game.state import State
+
+from knucklebones_ai.game.board import Board
+from knucklebones_ai.game.state import Player, State
+from .board_utils import display_state
 
 
 def clear_console():
     sp.run("cls" if os.name == "nt" else "clear")
+
+
+def wait_for_key():
+    print("Press ENTER to continue")
+    input()
 
 
 def run_cli():
@@ -14,22 +22,36 @@ def run_cli():
     while not finished:
         clear_console()
         if game is not None:
-            game.display()
+            display_state(game)
         else:
             print("No game loaded.")
-        command = input("Enter command: ").strip()
+        print("")
+
+        try:
+            command = input("Enter command: ").strip()
+        except EOFError:
+            command = "exit"
 
         match command:
             case "exit":
-                print("Exiting...")
+                print("\nExiting...")
                 finished = True
 
             case "new":
-                game = State()
+                match input("Who is the starting player? player or opponent (p/o): ").strip().lower():
+                    case "p":
+                        player = Player.PLAYER_ROLL
+                    case "o":
+                        player = Player.OPPONENT_ROLL
+                    case _:
+                        print("Invalid value")
+                        wait_for_key()
+                        continue
+
+                game = State(player, 0, Board(), Board())
 
             case "debug":
-                print("Press ENTER to continue")
-                input()
+                wait_for_key()
             
             case _:
                 print("Unknown command")
